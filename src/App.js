0323d7587a4e56c3f9bea5ad21004c64b3cea269
Katja Roehlig <katja-roehlig@mail.de>
2023-07-13 17:25:08 +0200

    initializing memory cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //Math.random liefert Zahlen zwischen 0 und 1
+  //sort() vergleicht 2 Items(a,b) und gibt einen Wert zwischen -0.5 und 0.5 zurück.
+  //ist der Wert < 0 , a,b bleiben in der Reihenfolge
+  //ist der Wert > 0 a,b wechseln die Reihenfolge
+  const getCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={() => getCards()}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
