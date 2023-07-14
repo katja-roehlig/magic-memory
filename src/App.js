3c7f5750a9b9d94e5ad497dac691d6a6938f37f4
Katja Roehlig <katja-roehlig@mail.de>
2023-07-14 15:37:08 +0200

    feat: adding a match-function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
+import { useEffect } from "react";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //Math.random liefert Zahlen zwischen 0 und 1
   //sort() vergleicht 2 Items(a,b) und gibt einen Wert zwischen -0.5 und 0.5 zurück.
@@ -26,18 +30,39 @@ function App() {
     setTurns(0);
   };
 
+  const handleChoice = (card) => {
+    //Wenn der Wert von choiceOne null ist, gibt es false zurück, wenn er gesetzt ist, kommt true zurück
+
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        choiceOne.matched = true;
+        choiceTwo.matched = true;
+        resetTurn();
+      } else {
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={() => getCards()}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="/img/cover.png" alt="card back" />
-            </div>
-          </div>
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
